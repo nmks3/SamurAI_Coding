05a8269603e8fe8d14dec7837dd94bbbd9f6f67c
nmks3 <nmks33@gmail.com>
2017-01-30 19:32:45 +0900

    32*32用に修正

--- manager/make_label_directory.sh
+++ manager/make_label_directory.sh
@@ -1,7 +1,7 @@
 dir_name=/Users/g-2017/SamurAI_Coding
 side=first
-arm=2
-cat ${dir_name}/Python/label1-1000.txt ${dir_name}/Python/label1001-2000.txt ${dir_name}/Python/label2001-3000.txt ${dir_name}/Python/label3001-4000.txt ${dir_name}/Python/label4001-5000.txt ${dir_name}/Python/label5001-6000.txt ${dir_name}/Python/label6001-7000.txt ${dir_name}/Python/label7001-8000.txt ${dir_name}/Python/label8001-9000.txt ${dir_name}/Python/label9001-10000.txt > ${dir_name}/manager/label.txt
+arm=0
+cat ${dir_name}/traindata/${side}/labels/label*.txt > ${dir_name}/manager/label.txt
 sort ${dir_name}/manager/label.txt | uniq > ${dir_name}/manager/label_0_${arm}.txt
 
 while read LINE; do

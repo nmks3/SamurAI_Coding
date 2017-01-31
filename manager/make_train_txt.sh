f569f068ff5fcdc81b15b54f0944a71a977f9fcc
nmks3 <nmks33@gmail.com>
2017-01-31 10:24:02 +0900

    シェルスクリプト修正

--- manager/make_train_txt.sh
+++ manager/make_train_txt.sh
@@ -1,22 +1,21 @@
-dir_name=/Users/g-2017/SamurAI_Coding/traindata
+dir_name=/Users/install/ML_inoue/SamurAI_Coding/traindata
 side=first
 arm=2
 
 cd ${dir_name}/${side}/${arm}/
-
-ls > ./filename_label.txt
+ls > ../filename_label.txt
 
 n=0
 while read LINE; do
     COLM1=`echo ${LINE} | cut -d , -f 1`
     if [ -n "$COLM1" ] ; then
-        ls ./$COLM1 > ./filename_label1.txt
+        ls ./$COLM1 > ../filename_label${arm}_1.txt
         while read LINE; do
         	COLM2=`echo ${LINE} | cut -d , -f 1`
         	if [ -n "$COLM2" ] ; then
         		echo ${dir_name}/${side}/${arm}/${COLM1}/${COLM2} ${n} 
         	fi
-        done < ./filename_label1.txt
+        done < ../filename_label${arm}_1.txt
     fi
     n=$((n+1))
-done < ./filename_label.txt
+done < ../filename_label${arm}.txt
--- manager/resize_image.sh
+++ manager/resize_image.sh
@@ -1,10 +1,15 @@
 dir_name=/Users/g-2017/SamuraiCoding
 side=first
 arm=0
-
+n=0
 while read LINE; do
     COLM1=`echo ${LINE} | cut -d , -f 1`
 	if [ -n "$COLM1" ] ; then
 		python ${dir_name}/Python/resize_image.py $COLM1
-	fi
+		m=$((n % 1000))
+		if [ $m -eq 0 ] ; then
+			echo $COLM1 | cut -c 47-51
+		fi
+		n=$((n+1))
+　	fi
 done < ${dir_name}/traindata/${side}/${arm}/train.txt

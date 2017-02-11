367a1452c070a85e3ec5ab4d779a740666d4045d
nmks3 <nmks33@gmail.com>
2017-02-11 15:31:04 +0900

    大会用コードにモデル構築の部分を組み込みエラーが出なかったのでコミット

--- pythonplayer/main.py
+++ pythonplayer/main.py
@@ -3,6 +3,7 @@ import sys
 import string
 import cv2
 import numpy as np
+import tensorflow as tf
 
 # import player
 import players.playerlist as player
@@ -11,15 +12,82 @@ import players.playerlist as player
 cols = 15
 # 行を設定
 rows = 15
+# プレイヤー
 num_players = 6
-tate = "/Users/g-2017/SamuraiCoding/Python/tate_1.png"
-yoko = "/Users/g-2017/SamuraiCoding/Python/yoko_1.png"
+# クラス数
+NUM_CLASSES = 70
+# 画像サイズ
+IMAGE_SIZE = 28
+# pixel計算
+IMAGE_PIXELS = IMAGE_SIZE*IMAGE_SIZE*3
+# 余白画像
+tate = "./tate_1.png"
+yoko = "./yoko_1.png"
 
 def ReadCommentedIStream(cis):
     l = cis.readline()
     while l.startswith('#'):
         l = cis.readline()
     return string.strip(l).split()
+
+def inference(images_placeholder, keep_prob):
+    """ モデルを作成する関数
+
+    引数:
+      images_placeholder: inputs()で作成した画像のplaceholder
+      keep_prob: dropout率のplace_holder
+
+    返り値:
+      cross_entropy: モデルの計算結果
+    """
+    def weight_variable(shape):
+      initial = tf.truncated_normal(shape, stddev=0.1)
+      return tf.Variable(initial)
+
+    def bias_variable(shape):
+      initial = tf.constant(0.1, shape=shape)
+      return tf.Variable(initial)
+
+    def conv2d(x, W):
+      return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+    def max_pool_2x2(x):
+      return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                            strides=[1, 2, 2, 1], padding='SAME')
+
+    x_image = tf.reshape(images_placeholder, [-1, 28, 28, 3])
+
+    with tf.name_scope('conv1') as scope:
+        W_conv1 = weight_variable([5, 5, 3, 32])
+        b_conv1 = bias_variable([32])
+        h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+
+    with tf.name_scope('pool1') as scope:
+        h_pool1 = max_pool_2x2(h_conv1)
+
+    with tf.name_scope('conv2') as scope:
+        W_conv2 = weight_variable([5, 5, 32, 64])
+        b_conv2 = bias_variable([64])
+        h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+
+    with tf.name_scope('pool2') as scope:
+        h_pool2 = max_pool_2x2(h_conv2)
+
+    with tf.name_scope('fc1') as scope:
+        W_fc1 = weight_variable([7*7*64, 1024])
+        b_fc1 = bias_variable([1024])
+        h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+        h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+        h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+    with tf.name_scope('fc2') as scope:
+        W_fc2 = weight_variable([1024, NUM_CLASSES])
+        b_fc2 = bias_variable([NUM_CLASSES])
+
+    with tf.name_scope('softmax') as scope:
+        y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+    return y_conv
 
 class SamuraiInfo:
     """Samurai Information"""
@@ -72,7 +140,7 @@ class GameInfo:
         self.field = [[0 for i in range(self.size[1])] for j in range(self.size[0])]
         # イメージ生成
         image = np.zeros((rows, cols, 3), np.uint8)
-        outimg = "../" + str(self.turn) + "out.png"
+        outimg = "/Users/g-2017/Samuraicoding/" + str(self.turn) + "out.png"
         # 画像の初期化
         image[0:15, 0:15] = [255, 255, 255]
         
@@ -105,7 +173,7 @@ class GameInfo:
         img5 = cv2.imread(yoko)
         #縦連結 30x30 30x1
         img6 = cv2.vconcat([img4,img5])
-        #90x93
+        #30x31
         #縦連結 30x1 30x31
         img7 = cv2.vconcat([img5,img6])
         #30x32
@@ -118,7 +186,8 @@ class GameInfo:
         img10 = cv2.hconcat([img8,img9])
         # 32x32
         # 画像の出力
-        cv2.imwrite(outimg, img10)
+        img = cv2.resize(img10, (28, 28))
+        cv2.imwrite(outimg, img)
 
     def isValid(self, action):
         myself = self.samuraiInfo[self.weapon]
